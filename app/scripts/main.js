d6159e337f11fe82b7419c1ab7c0c3b02720bfb7
=Ben Roth <ben@benatwork.me>
2013-02-28 13:48:50 -0500

    removed whitespace from grunt jshint

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,36 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+/*global $:false */
+$(document).ready(function() {
+	$('#message-submit').click(function () {
+		var inputField = $('#message-input');
+		var message = inputField.val();
+
+
+		inputField.val('');
+
+		var newLi = $('<li>'+message+'</li>').prependTo($('.feed')).addClass('new-li');
+
+		setTimeout(function () {
+			newLi.removeClass('new-li');
+		},0);
+
+		// $.getJSON(
+		// 	"https://api.twitter.com/1.1/statuses/show.json?id=210462857140252672&callback=twitterCallback",
+		// 	function(data){
+		// 		console.log(data);
+		// 	});
+
+
+		// setTimeout(function(){
+		// 	newLi.text(message);
+		// },2000);
+
+	});
+	// function updateDelays(){
+	// 	$('ul li').each(function(i){
+	// 		var t = $(this);
+	// 	    setTimeout(function(){ t.addClass('animation'); }, (i+1) * 50);
+	// 	});
+
+	// }
+});
